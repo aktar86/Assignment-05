f37e1adabf2d7a04a1dd342a1c59fe7c2974c913
Md Aktar Ahmed <amdaktar86@gmail.com>
2025-08-28 18:40:00 +0600

    added all function

---
+++ README.md
@@ -0,0 +1,52 @@
+# JavaScript Concepts - Notes
+
+## Ans-01: Difference between getElementById, getElementsByClassName, querySelector, and querySelectorAll
+
+- `getElementById('id')`  
+  Selects a **single element** with the given ID. Returns a single element.
+
+- `getElementsByClassName('class')`  
+  Selects **all elements** with the given class. Returns an **HTMLCollection** (live collection).
+
+- `querySelector('selector')`  
+  Selects the **first element** that matches a CSS selector.
+
+- `querySelectorAll('selector')`  
+  Selects **all elements** matching the CSS selector. Returns a **NodeList**.
+
+---
+
+## Ans-02: How to create and insert a new element into the DOM
+
+```html
+<body>
+  <div id="parent"></div>
+
+  <script>
+    const newDiv = document.createElement("div");
+    newDiv.className = "html-container";
+    newDiv.innerHTML = `
+            <h1 style="text-align: center; font-weight: bold;">Hello World</h1>
+        `;
+    const addHtml = document.getElementById("parent");
+    addHtml.appendChild(newDiv);
+  </script>
+</body>
+```
+
+## Ans-03: Event Bubbling:
+
+`When an event occurs on an element, it bubbles up from the target element to its ancestors.`
+Example: _Button click_ করলে প্রথমে _button_ এর _handler_ চলে _parent grandparent_ শেষ পর্যন্ত `<body>` বা `<html>` এ পৌঁছায়।
+
+## Ans-04: Event Delegation:
+
+অনেক **child element** এ listener দেয়ার পরিবর্তে, **parent element** এ একটি **listener** বসানো হয় এবং **event.target** ব্যবহার করে **child handle** করা হয়। এর উপকারিতা হলো **Memory** কম লাগে এবং **Dynamic element** এর জন্যও কাজ করে
+
+## Ans-5: Difference between preventDefault() and stopPropagation()-
+
+1. `preventDefault(): Event এর default behavior আচরণ বন্ধ করে।`
+   যেমন: <a> link click করলে normally page navigate করে, কিন্তু preventDefault() দিলে navigate বন্ধ হবে।
+
+2. `stopPropagation(): Event bubbling বন্ধ করে।`
+   যেমন: **Child element click** করলে parent handler fire হবে না যদি **stopPropagation()** use করা হয়।
--- script.js
+++ script.js
@@ -1,25 +1,56 @@
 // available icon function
-function availAbleCoin(id){
+function availAbleCoin(id, number, text){
     const coin = document.getElementById(id).innerText;
     const coinNumber = parseInt(coin)
+    
     if (coinNumber < 20) {
-        alert("You don’t have enough coins to continue.");
-        return;
+        alert(`You don’t have enough coins to continue.` );
+        return false;
     }
     else {
-        alert('Call ongoin');
+        alert(`📞 ${text} Emergency Calling... ${number}`);
     }
     const upDateAvailableCoin = coinNumber - 20;
     document.getElementById(id).innerText = upDateAvailableCoin;
+    return true;
 }
 
-// call history update 
+// call button handler
+function callHandler(titleId, numberId, textId){
+    const title = document.getElementById(titleId).innerText;
+    const number = document.getElementById(numberId).innerText;
+    const text = document.getElementById(textId).innerText;
+    //available icon and alert
+    const coin = availAbleCoin('available-coin', number, text);
+    if(coin){
+        // update call history
+        updateCallHistory(title, number)
+    }
+}
+
+function getTime() {
+    const now = new Date();
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+    let ampm;
+
+    if (h >= 12) {ampm = "PM";} else { ampm = "AM"; }
+
+    h = h % 12;
+    if (h === 0) {h = 12;}
+    if (m < 10) { m = "0" + m;}
+    if (s < 10) {s = "0" + s;}
+
+    const time = h + ":" + m + ":" + s + " " + ampm;
+    return time;
+}
+
+// Call history update Card
 function updateCallHistory(title, number){
     const time = getTime();
-
     const newCard = document.createElement('div');
     newCard.className = "flex justify-between items-center bg-[#FAFAFA] p-1 rounded-lg mb-5";
-
     newCard.innerHTML = `
                         <div>
                             <h1 class="font-bold text-lg">${title}</h1>
@@ -29,30 +60,14 @@ function updateCallHistory(title, number){
                             <p class="text-[16px]">${time}</p>
                         </div>
             `;
-
     const historyContainer = document.getElementById('history-container');
+
+    if(historyContainer.children.length >= 5){
+        historyContainer.removeChild(historyContainer.firstChild)
+    }
+
     historyContainer.appendChild(newCard);
 }
-
-// copy button feature
-function copyNumber(id){
-        const number = document.getElementById(id).innerText;
-    navigator.clipboard.writeText(number)
-        .then(() => {
-            alert("Number Copied Successfully: " + number)
-        }).catch(err => {
-            console.error("Copy failed: ", err);
-        })
-
-    // copy-count
-    const copyCount = document.getElementById('copy-count').innerText;
-    const copyCountNumber = parseInt(copyCount)
-    let copyCountIncrese = copyCountNumber;
-    copyCountIncrese++
-    document.getElementById('copy-count').innerText = copyCountIncrese;
-}
-
-
 
 // all card heart function here
 const hearts = document.getElementsByClassName('heart');
@@ -66,165 +81,52 @@ for(const heart of hearts){
 }
 
 
+const cardNames = [
+  "national", "police", "fire", "ambulance",
+  "womanchild", "corruption", "electricity",
+  "brack", "railway"
+];
+
+cardNames.forEach(name => {
+  document.getElementById(`${name}-call-btn`).addEventListener('click', () => {
+    callHandler(`${name}-title`, `${name}-Service-number`, `${name}-text`);
+  });
+});
+
+
+
+// copy button feature
+function copyNumber(id){
+    const number = document.getElementById(id).innerText;
+    navigator.clipboard.writeText(number)
+        .then(() => {
+            alert("Copied to clipboard: " + number)
+        }).catch(err => {
+            console.error("Copy failed: ", err);
+        })
+
+    // copy-count
+    const copyCount = document.getElementById('copy-count').innerText;
+    const copyCountNumber = parseInt(copyCount)
+    let copyCountIncrese = copyCountNumber;
+    copyCountIncrese++
+    document.getElementById('copy-count').innerText = copyCountIncrese;
+}
+
+const copyButton = [
+  "national", "police", "fire", "ambulance",
+  "womanchild", "corruption", "electricity",
+  "brack", "railway"
+];
+
+copyButton.forEach(copy => {
+    document.getElementById(`${copy}-number-copy`).addEventListener('click', function(){
+        copyNumber(`${copy}-Service-number`)
+    })
+})
+
 // clear log history 
 document.getElementById('clear-log-btn').addEventListener('click', function () {
     // console.log('clear button clicked')
     document.getElementById('history-container').innerHTML = '';
 })
-
-
-// time function
-function getTime() {
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-
-    // am/pm check
-    let ampm;
-    if (h >= 12) { ampm = 'PM'; } else { ampm = 'AM'; }
-
-    // minutes check
-    if (m < 10) { m = "0" + m; } else { m = m.toString(); }
-
-    // second check
-    if (s < 10) { s = "0" + s; } else { s = s.toString(); }
-
-    const time = h + ":" + m + ":" + s + ' ' + ampm;
-    return time;
-}
-
-
-
-
-// card 1
-document.getElementById('national-call-btn').addEventListener('click', function () { 
-    availAbleCoin('available-coin')
-    // update call history
-    const title = document.getElementById('national-title').innerText;
-    const number = document.getElementById('national-Service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-
-// car 2
-document.getElementById('police-call-btn').addEventListener('click', function () {
-    availAbleCoin('available-coin')
-    // update call history here
-    const title = document.getElementById('police-title').innerText;
-    const number = document.getElementById('police-service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-// card 3
-document.getElementById('fire-call-btn').addEventListener('click', function () { 
-    availAbleCoin('available-coin')
-    // update call history
-    const title = document.getElementById('fire-title').innerText;
-    const number = document.getElementById('fire-Service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-
-// card 4
-document.getElementById('ambulance-call-btn').addEventListener('click', function () { 
-    availAbleCoin('available-coin')
-    // update call history
-    const title = document.getElementById('ambulance-title').innerText;
-    const number = document.getElementById('ambulance-Service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-// card 5
-document.getElementById('womanchild-call-btn').addEventListener('click', function () { 
-    availAbleCoin('available-coin')
-    // update call history
-    const title = document.getElementById('womanchild-title').innerText;
-    const number = document.getElementById('womanchild-Service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-// card 6
-document.getElementById('corruption-call-btn').addEventListener('click', function () { 
-    availAbleCoin('available-coin')
-    // update call history
-    const title = document.getElementById('corruption-title').innerText;
-    const number = document.getElementById('corruption-Service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-
-// card 7
-document.getElementById('Electricity-call-btn').addEventListener('click', function () { 
-    availAbleCoin('available-coin')
-    // update call history
-    const title = document.getElementById('Electricity-title').innerText;
-    const number = document.getElementById('Electricity-Service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-
-// card 8
-document.getElementById('brack-call-btn').addEventListener('click', function () { 
-    availAbleCoin('available-coin')
-    // update call history
-    const title = document.getElementById('brack-title').innerText;
-    const number = document.getElementById('brack-Service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-// card 9
-document.getElementById('railway-call-btn').addEventListener('click', function () { 
-    availAbleCoin('available-coin')
-    // update call history
-    const title = document.getElementById('railway-title').innerText;
-    const number = document.getElementById('railway-Service-number').innerText;
-    updateCallHistory(title, number)
-})
-
-
-//card 1 copy button
-document.getElementById('national-number-copy').addEventListener('click', function () {
-        copyNumber('national-Service-number')
-})
-
-// card 2 copy button
-document.getElementById('police-number-copy').addEventListener('click', function () {
-        copyNumber('police-service-number')
-})
-
-//card 3 copy button
-document.getElementById('fire-number-copy').addEventListener('click', function () {
-        copyNumber('fire-Service-number')
-})
-
-//card 4 copy button
-document.getElementById('ambulance-number-copy').addEventListener('click', function () {
-        copyNumber('ambulance-Service-number')
-})
-
-//card 5 copy button
-document.getElementById('womanchild-number-copy').addEventListener('click', function () {
-        copyNumber('womanchild-Service-number')
-})
-
-//card 6 copy button
-document.getElementById('corruption-number-copy').addEventListener('click', function () {
-        copyNumber('corruption-Service-number')
-})
-
-//card 7 copy button
-document.getElementById('Electricity-number-copy').addEventListener('click', function () {
-        copyNumber('Electricity-Service-number')
-})
-
-//card 8 copy button
-document.getElementById('brack-number-copy').addEventListener('click', function () {
-        copyNumber('brack-Service-number')
-})
-
-//card 9 copy button
-document.getElementById('railway-number-copy').addEventListener('click', function () {
-        copyNumber('railway-Service-number')
-})
-
-
